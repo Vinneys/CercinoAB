ebe75673927715db101e48800faf46e8867bfe7e
Vinneys <Vincehorvath32@gmail.com>
2025-10-07 14:14:58 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        // Use default adapter-static output directory "build" for clarity
-        adapter: adapter({
-            fallback: '404.html'
-        }),
+        // SPA fallback to index.html to work on static hosting
+        adapter: adapter({ fallback: 'index.html' }),
         prerender: {
             handleHttpError: 'warn'
         }
